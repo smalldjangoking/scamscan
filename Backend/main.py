22ba685c410abbaeee2d35a904706b188faa2c62
smalldjangoking <pyclanp@gmail.com>
2025-09-02 15:28:55 +0300

    CORS

--- Backend/main.py
+++ Backend/main.py
@@ -1,7 +1,17 @@
 from fastapi import FastAPI
 from router import auth
+from fastapi.middleware.cors import CORSMiddleware
+
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Можно указать список доменов вместо ["*"]
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 async def read_root():
