80eac02fa47f65a30f7f81cb6a90c4fb11b1eea6
Ruslan <pyclanp@Gmail.com>
2025-12-10 01:08:47 +0200

    minor fixes

--- client/src/components/ui/loading.jsx
+++ client/src/components/ui/loading.jsx
@@ -1,15 +1,13 @@
 import React from 'react';
-import { Loader2 } from 'lucide-react'; // Lucide icon для загрузки
+import { Loader2 } from 'lucide-react';
 
 const LoadingSpinner = () => {
   return (
     <div role="status" aria-label="loading" className="flex flex-col items-center justify-center py-10">
-      {/* Иконка Lucide с анимацией */}
       <Loader2 
         className="max-w-[35px] max-h-[35px]  w-full h-full animate-spin text-[color:var(--secondary-foreground]" 
       />
 
-      {/* Текст для скринридеров */}
       <span className="sr-only">Loading proccess animation</span>
     </div>
   );
