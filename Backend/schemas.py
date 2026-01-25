799740b1e85cbef8a1931f5fed35b1a40825f129
Ruslan <pyclanp@Gmail.com>
2026-01-25 17:39:00 +0200

    New validator for password field

--- Backend/schemas.py
+++ Backend/schemas.py
@@ -1,8 +1,17 @@
 from datetime import datetime
 from enum import Enum
-from pydantic import BaseModel, EmailStr, model_validator, ConfigDict, Field, field_validator
+from pydantic import (
+    BaseModel,
+    EmailStr,
+    model_validator,
+    ConfigDict,
+    Field,
+    field_validator,
+)
 from typing import Annotated
 from services import normalize_url, normalize_description, nickname_symbols_check
+import re
+
 
 class UserRegistrationSchema(BaseModel):
     email: EmailStr
@@ -14,24 +23,36 @@ class UserRegistrationSchema(BaseModel):
     def password_check(cls, data):
         if data.password != data.password2:
             raise ValueError("Passwords do not match")
-        
+
         if len(data.password) < 8:
             raise ValueError("Password must be at least 8 characters.")
-        
+
         if len(data.password) > 64:
             raise ValueError("Password must be at less 64 characters.")
-        
+
         if len(data.nickname) < 3:
             raise ValueError("Nickname must be at least 3 characters.")
-        
+
         if len(data.nickname) > 15:
             raise ValueError("Nickname must be at most 15 characters.")
-        
+
         if not nickname_symbols_check(data.nickname):
             raise ValueError("Nickname can contain only English letters and numbers")
-        
+
         return data
 
+    @field_validator("password")
+    def password_rules(cls, v: str) -> str:
+        if not re.search(r"\d", v):
+            raise ValueError("Password must contain at least one digit.")
+
+        if not re.search(r"[A-Z]", v):
+            raise ValueError("Password must contain at least one uppercase letter.")
+
+        if not re.search(r"[a-z]", v):
+            raise ValueError("Password must contain at least one lowercase letter.")
+
+        return v
 
 
 class UserLoginSchema(BaseModel):
@@ -53,6 +74,7 @@ class UpdateUserInfoSchema(BaseModel):
     surname: str | None = None
     nickname: str | None = None
 
+
 class UserVote(BaseModel):
     value: int
 
@@ -61,6 +83,7 @@ class UserVote(BaseModel):
         if data.value not in [-1, 1]:
             raise ValueError("Vote must be only in two positions")
         return data
+
 
 class ReportSchema(BaseModel):
     subject: str
@@ -75,28 +98,33 @@ class ReportSchema(BaseModel):
     def both_subjects_not_allowed(cls, data):
         if data.subject == "crypto" and (data.website_url is not None):
             raise ValueError("website_url must be None when subject is 'crypto'")
-        if data.subject == "website" and (data.crypto_address is not None or data.crypto_name is not None):
-            raise ValueError("crypto_address and crypto_name must be None when subject is 'website'")
+        if data.subject == "website" and (
+            data.crypto_address is not None or data.crypto_name is not None
+        ):
+            raise ValueError(
+                "crypto_address and crypto_name must be None when subject is 'website'"
+            )
         return data
-    
+
     @model_validator(mode="after")
     def subject_one_of_two(cls, data):
         if data.subject not in ["crypto", "website"]:
             raise ValueError("subject must be either 'crypto' or 'website'")
         return data
-    
+
     @field_validator("website_url", mode="after")
     def normalize_website_url(cls, v):
         if v is not None:
             return normalize_url(v)
         return v
-    
+
     @field_validator("report_description", mode="after")
     def normalize_report_description(cls, v):
         if v is not None:
             return normalize_description(v)
         return v
-    
+
+
 class WhoisSchema(BaseModel):
     model_config = ConfigDict(from_attributes=True)
 
@@ -132,21 +160,24 @@ class ReportAPISchema(BaseModel):
     created_at: datetime
     views: int
 
+
 class ReportsListAPISchema(BaseModel):
     reports: list[ReportAPISchema]
     totalPages: int
+
 
 class AddressReportSchema(BaseModel):
     model_config = ConfigDict(from_attributes=True)
 
     id: int
-    report_title: str 
+    report_title: str
     report_description: str
     address_id: int | None = None
     slug: str
     created_at: datetime
     views: int
     comments_count: int | None = None
+
 
 class AddressListReportSchema(BaseModel):
     reports: list[AddressReportSchema]
@@ -171,21 +202,25 @@ class SingleReport(BaseModel):
     created_at: datetime
     views: int
 
+
 class PublicUserAPISchema(BaseModel):
     model_config = ConfigDict(from_attributes=True)
 
     user_id: int = Field(alias="id")
     nickname: str
 
+
 class SingleReportSchema(BaseModel):
     address: AddressAPISchema
     report: SingleReport
     user: PublicUserAPISchema | None = None
 
+
 class CommentUser(BaseModel):
     model_config = ConfigDict(from_attributes=True)
 
     nickname: str
+
 
 class CommentSchema(BaseModel):
     model_config = ConfigDict(from_attributes=True)
@@ -194,8 +229,9 @@ class CommentSchema(BaseModel):
     user_id: int
     comment: str
     created_at: datetime
-    children: list['CommentReply'] | None = None
+    children: list["CommentReply"] | None = None
     user: CommentUser
+
 
 class CommentReply(BaseModel):
     model_config = ConfigDict(from_attributes=True)
@@ -212,22 +248,15 @@ class CommentsSchema(BaseModel):
     comments_total: int
     total_pages: int
 
+
 class CommentValid(BaseModel):
     parent_comment_id: int | None = None
     comment: str
 
+
 class PasswordRestore(BaseModel):
     password: str | None = None
 
+
 class EmailRequest(BaseModel):
     email: EmailStr
-
-
-
-
-
-
-
-
-
-    
