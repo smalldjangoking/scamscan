ece5edfcbf7fe1062fd3fd69382f5faf4ee3a91a
Ruslan <pyclanp@gmail.com>
2026-02-28 00:27:16 +0200

    fix: docker-compose, dockerfile fixes

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -1,13 +1,9 @@
 #!/bin/sh
 set -e
 
-echo "Waiting for Postgres..."
-until pg_isready -h "$POSTGRES_HOST" -p "$POSTGRES_PORT" -U "$POSTGRES_USER"; do
-  sleep 1
-done
-
-echo "Applying Alembic migrations..."
+echo "➡️ Применение миграций базы данных (Alembic)..."
 alembic upgrade head
 
-echo "Starting Uvicorn..."
-uvicorn main:app --host 0.0.0.0 --port 443 --ssl-certfile /certs/cert.pem --ssl-keyfile /certs/key.pem
+echo "➡️ Запуск Uvicorn сервера..."
+
+exec uvicorn main:app --host 0.0.0.0 --port 443 --ssl-certfile /certs/cert.pem --ssl-keyfile /certs/key.pem
